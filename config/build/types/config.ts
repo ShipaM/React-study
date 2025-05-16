5455cdcb68b3f73bb53294c6af4e8febd269314e
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-05-16 20:22:17 +0300

    feat(deploy to production): deploy project

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -5,6 +5,8 @@ export interface BuildPaths {
   build: string;
   html: string;
   src: string;
+  locales: string;
+  buildLocales: string;
 }
 
 export interface BuildEnv {
@@ -19,7 +21,7 @@ export interface BuildOptions {
   isDev: boolean;
   port: number;
   apiUrl: string;
-  project: 'storybook'| 'frontend' | 'jest'
+  project: "storybook" | "frontend" | "jest";
 }
 
 export declare const __IS_DEV__: boolean;
